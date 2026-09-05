7e7d0b84fc252b90d90982f550585d1c65e4d040
mycomind4-arch <mycomind4@gmail.com>
2026-09-05 10:49:46 -0700

    wire Tenant Reply shared public landing

--- apps/verticals/tenant-reply/src/main.tsx
+++ apps/verticals/tenant-reply/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Root from './Root'
+import '../../../../packages/design-system/src/tokens.css'
+import '../../../../packages/design-system/src/patterns.css'
 import './styles/globals.css'
+
+document.body.dataset.mmpTheme = 'tenant-reply'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 )
