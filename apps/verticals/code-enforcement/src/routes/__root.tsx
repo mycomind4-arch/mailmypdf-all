1348cd12e4070fe8d71a9cc958d428b226b9de29
mycomind4-arch <mycomind4@gmail.com>
2026-09-05 13:09:46 -0700

    style(code-enforcement): adopt shared public design system

--- apps/verticals/code-enforcement/src/routes/__root.tsx
+++ apps/verticals/code-enforcement/src/routes/__root.tsx
@@ -1,5 +1,8 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { AuthProvider } from '../lib/auth'
+import '../../../../../packages/design-system/src/tokens.css'
+import '../../../../../packages/design-system/src/patterns.css'
 import '../styles.css'
+
 export const Route=createRootRoute({component:Root})
-function Root(){return <AuthProvider><Outlet/></AuthProvider>}
+function Root(){return <AuthProvider><div data-mmp-theme='code-enforcement'><Outlet/></div></AuthProvider>}
