a08dcf04bdc76fac4b83376fe75549cfd2d7a6e2
mycomind4-arch <mycomind4@gmail.com>
2026-09-05 07:15:37 -0700

    wire Records Requests into shared hero system

--- apps/verticals/records-request/src/routes/__root.tsx
+++ apps/verticals/records-request/src/routes/__root.tsx
@@ -1,5 +1,11 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import "../../../../../packages/design-system/src/tokens.css"
+import "../../../../../packages/design-system/src/patterns.css"
 import { AuthProvider } from '../lib/auth'
 import '../styles.css'
+
 export const Route=createRootRoute({component:Root})
-function Root(){return <AuthProvider><Outlet/></AuthProvider>}
+
+function Root(){
+  return <div data-mmp-theme="records-request"><AuthProvider><Outlet/></AuthProvider></div>
+}
