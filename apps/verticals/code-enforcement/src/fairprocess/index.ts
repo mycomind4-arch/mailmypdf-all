d352435e6eda1b893340f99d6e1a519ca2c5e1a0
mycomind4-arch <mycomind4@gmail.com>
2026-09-07 22:49:27 -0700

    feat(code-enforcement): export connector evidence persistence

--- apps/verticals/code-enforcement/src/fairprocess/index.ts
+++ apps/verticals/code-enforcement/src/fairprocess/index.ts
@@ -5,6 +5,7 @@ import { HUMBOLDT_FAIRPROCESS_PACK } from './jurisdictions/humboldt';
 export * from './types';
 export * from './attorney-packet';
 export * from './connector';
+export * from './connector-persistence';
 export * from './evidence-integrity';
 export * from './investigation-adapter';
 export * from './jurisdiction-registry';
