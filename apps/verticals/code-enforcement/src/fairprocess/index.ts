15583e8f713c66eb11a2908ce4477dfbaf2a90ab
mycomind4-arch <mycomind4@gmail.com>
2026-09-07 22:37:48 -0700

    feat(code-enforcement): expose FairProcess integration entrypoint

---
+++ apps/verticals/code-enforcement/src/fairprocess/index.ts
@@ -0,0 +1,24 @@
+import type { Jurisdiction } from '../domain/jurisdiction';
+import { resolveJurisdictionPack, registerJurisdictionPack } from './jurisdiction-registry';
+import { HUMBOLDT_FAIRPROCESS_PACK } from './jurisdictions/humboldt';
+
+export * from './types';
+export * from './attorney-packet';
+export * from './jurisdiction-registry';
+export * from './jurisdictions/humboldt';
+
+export function ensureFairProcessJurisdictions(): void {
+  registerJurisdictionPack(HUMBOLDT_FAIRPROCESS_PACK);
+}
+
+export function resolveFairProcessPackForJurisdiction(
+  jurisdiction: Pick<Jurisdiction, 'state' | 'county' | 'municipality' | 'agency'>,
+) {
+  ensureFairProcessJurisdictions();
+  return resolveJurisdictionPack({
+    state: jurisdiction.state,
+    county: jurisdiction.county,
+    municipality: jurisdiction.municipality,
+    agency: jurisdiction.agency,
+  });
+}
