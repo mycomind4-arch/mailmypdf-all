b117d3358c4700da0c7f3498f6f6b2806b9d7b0d
mycomind4-arch <mycomind4@gmail.com>
2026-09-09 10:51:52 -0700

    upgrade(records): wire production criminal records workflow

--- apps/verticals/records-request/src/workflows/index.ts
+++ apps/verticals/records-request/src/workflows/index.ts
@@ -11,6 +11,7 @@ import { policePursuitRecordsWorkflow } from './police-pursuit-records'
 import { officerInvolvedShootingRecordsWorkflow } from './officer-involved-shooting-records'
 import { evidencePropertyRoomRecordsWorkflow } from './evidence-property-room-records'
 import { productionArrestRecordsWorkflow } from './arrest-records-production'
+import { productionCriminalRecordsWorkflow } from './criminal-records-production'
 import { propertyPermitRecordsWorkflow } from './property-permit-records'
 import { governmentCommunicationsRecordsWorkflow } from './government-communications-records'
 import { foiaRequestWorkflow } from './foia-request'
@@ -20,7 +21,7 @@ import { planningRecordsWorkflow } from './planning-records'
 import { publicRecordsRequestWorkflow } from './public-records-request'
 import { caseRecordsWorkflow } from './case-records'
 import { birthRecordsWorkflow, marriageRecordsWorkflow, divorceRecordsWorkflow, deathRecordsWorkflow } from './vital-records'
-import { criminalRecordsWorkflow, criminalHistoryWorkflow, backgroundCheckRecordsWorkflow } from './criminal-records'
+import { criminalHistoryWorkflow, backgroundCheckRecordsWorkflow } from './criminal-records'
 import { militaryRecordsWorkflow, medicalRecordsWorkflow, employmentRecordsWorkflow, educationRecordsWorkflow, recordsFollowUpWorkflow, recordsDenialAppealWorkflow } from './specialized-records'
 import { policeReportWorkflow, policeReportCopyWorkflow, publicInformationRequestWorkflow, openRecordsRequestWorkflow, agencyRecordsRequestWorkflow, governmentDocumentsRequestWorkflow } from './variant-records'
 import type { RecordsWorkflow } from '../workflow-factory'
@@ -48,7 +49,7 @@ export const recordsWorkflows: readonly RecordsWorkflow[] = [
   policeReportWorkflow,
   policeReportCopyWorkflow,
   courtRecordsWorkflow,
-  criminalRecordsWorkflow,
+  productionCriminalRecordsWorkflow,
   criminalHistoryWorkflow,
   productionArrestRecordsWorkflow,
   backgroundCheckRecordsWorkflow,
