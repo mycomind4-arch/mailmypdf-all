dfce41b49d88a8dbde65c19f71860311d0644935
mycomind4-arch <mycomind4@gmail.com>
2026-09-16 04:07:22 -0700

    feat(publishing): add index.ts

---
+++ packages/autonomous-publishing/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./types.js";
+export * from "./manifest.js";
+export * from "./evidence.js";
+export * from "./memory.js";
+export * from "./adapters.js";
+export * from "./pipeline.js";
