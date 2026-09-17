4103343d3f0707631ccf00bb46a78d2c01ce03d2
mycomind4-arch <mycomind4@gmail.com>
2026-09-17 14:02:12 -0700

    feat(appeal-mail): mount dental insurance denial start route

--- appeal-mail/workflows/appeal-dental-insurance-denial/start/index.tsx
+++ appeal-mail/workflows/appeal-dental-insurance-denial/start/index.tsx
@@ -1,7 +1,8 @@
+import { createFileRoute } from "@tanstack/react-router";
 import InsuranceAppealWorkflow from "../../../shared/InsuranceAppealWorkflow";
 import workflow from "../manifest";
 
-export default function DentalInsuranceDenialStart() {
+export function DentalInsuranceDenialStart() {
   return (
     <InsuranceAppealWorkflow
       config={{
@@ -13,3 +14,9 @@ export default function DentalInsuranceDenialStart() {
     />
   );
 }
+
+export const Route = createFileRoute("/appeal-mail/workflows/appeal-dental-insurance-denial/start/")({
+  component: DentalInsuranceDenialStart,
+});
+
+export default DentalInsuranceDenialStart;
