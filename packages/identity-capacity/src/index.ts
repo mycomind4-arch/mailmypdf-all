bb106d02edc2bac09b8b995bd0f32e374e084ece
mycomind4-arch <mycomind4@gmail.com>
2026-09-17 15:51:15 -0700

    feat(identity-capacity): export classification capacity and authority engines

--- packages/identity-capacity/src/index.ts
+++ packages/identity-capacity/src/index.ts
@@ -33,3 +33,44 @@ export {
   resolveAuthoritativeName,
   authoritativeNameResolutionToFinding,
 } from "./authoritative-name.js";
+
+export type {
+  EntityClassification,
+  EntityClassificationDisposition,
+  EntityClassificationSignal,
+  EvaluatedEntityClassificationSignal,
+  EntityClassificationResult,
+} from "./entity-classification.js";
+export {
+  ENTITY_CLASSIFICATION_RULES,
+  resolveEntityClassification,
+  entityClassificationToFinding,
+} from "./entity-classification.js";
+
+export type {
+  Capacity,
+  CapacityResolutionDisposition,
+  CapacityClaim,
+  EvaluatedCapacityClaim,
+  ResolvedCapacity,
+  CapacityResolutionResult,
+} from "./capacity-resolution.js";
+export {
+  CAPACITY_RESOLUTION_RULES,
+  resolveCapacity,
+  capacityResolutionToFinding,
+} from "./capacity-resolution.js";
+
+export type {
+  AuthorizationEffect,
+  AuthorityToActDisposition,
+  AuthorityAction,
+  AuthorizationEvidence,
+  EvaluatedAuthorizationEvidence,
+  AuthorityToActResult,
+} from "./authority-to-act.js";
+export {
+  AUTHORITY_TO_ACT_RULES,
+  resolveAuthorityToAct,
+  authorityToActToFinding,
+} from "./authority-to-act.js";
