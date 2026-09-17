f33c8c3618e7191ec4b4e48689c813a26aae6df4
mycomind4-arch <mycomind4@gmail.com>
2026-09-17 13:08:05 -0700

    feat(benefits): export benefits workflow registry

---
+++ benefits-appeal/workflows/index.ts
@@ -0,0 +1,12 @@
+export type {
+  BenefitsWorkflowMaturity,
+  BenefitsCanonicalWorkflow,
+  BenefitsWorkflowRegistryEntry,
+} from "./registry";
+
+export {
+  BENEFITS_APPEAL_WORKFLOWS,
+  getBenefitsWorkflowEntry,
+  getBenefitsWorkflowLaunchPath,
+  isBenefitsWorkflowExecutable,
+} from "./registry";
