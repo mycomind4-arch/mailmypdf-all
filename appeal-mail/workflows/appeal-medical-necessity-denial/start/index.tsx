3abdd091b95722d5af3d9532f55c2dbac7049b7e
mycomind4-arch <mycomind4@gmail.com>
2026-09-17 14:01:43 -0700

    feat(appeal-mail): mount medical necessity denial start route

--- appeal-mail/workflows/appeal-medical-necessity-denial/start/index.tsx
+++ appeal-mail/workflows/appeal-medical-necessity-denial/start/index.tsx
@@ -1,7 +1,8 @@
+import { createFileRoute } from "@tanstack/react-router";
 import InsuranceAppealWorkflow from "../../../shared/InsuranceAppealWorkflow";
 import workflow from "../manifest";
 
-export default function MedicalNecessityDenialStart() {
+export function MedicalNecessityDenialStart() {
   return (
     <InsuranceAppealWorkflow
       config={{
@@ -13,3 +14,9 @@ export default function MedicalNecessityDenialStart() {
     />
   );
 }
+
+export const Route = createFileRoute("/appeal-mail/workflows/appeal-medical-necessity-denial/start/")({
+  component: MedicalNecessityDenialStart,
+});
+
+export default MedicalNecessityDenialStart;
