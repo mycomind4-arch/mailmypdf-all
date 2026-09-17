4c1daa667271861d63dcb41d880866d1e039d5d1
mycomind4-arch <mycomind4@gmail.com>
2026-09-17 14:02:23 -0700

    feat(appeal-mail): mount life insurance denial start route

--- appeal-mail/workflows/appeal-life-insurance-denial/start/index.tsx
+++ appeal-mail/workflows/appeal-life-insurance-denial/start/index.tsx
@@ -1,7 +1,8 @@
+import { createFileRoute } from "@tanstack/react-router";
 import InsuranceAppealWorkflow from "../../../shared/InsuranceAppealWorkflow";
 import workflow from "../manifest";
 
-export default function LifeInsuranceDenialStart() {
+export function LifeInsuranceDenialStart() {
   return (
     <InsuranceAppealWorkflow
       config={{
@@ -13,3 +14,9 @@ export default function LifeInsuranceDenialStart() {
     />
   );
 }
+
+export const Route = createFileRoute("/appeal-mail/workflows/appeal-life-insurance-denial/start/")({
+  component: LifeInsuranceDenialStart,
+});
+
+export default LifeInsuranceDenialStart;
