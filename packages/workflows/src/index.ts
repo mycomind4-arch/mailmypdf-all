ee4572c1b048858c587a50d5a57da29cc5b1c2e3
mycomind4-arch <mycomind4@gmail.com>
2026-09-16 19:17:19 -0700

    feat: export shared workflow runtime server

--- packages/workflows/src/index.ts
+++ packages/workflows/src/index.ts
@@ -45,3 +45,4 @@ export * from "./workflow-conditions.js";
 export * from "./draft-validator.js";
 export * from "./matter-runtime-client.js";
 export * from "./matter-runtime.js";
+export * from "./matter-runtime-server.js";
