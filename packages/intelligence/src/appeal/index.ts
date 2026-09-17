3dd4f790c6f0a5c309ff84f85aae7d3a99446d7e
mycomind4-arch <mycomind4@gmail.com>
2026-09-17 12:11:48 -0700

    feat(intelligence): export shared appeal domain pack

---
+++ packages/intelligence/src/appeal/index.ts
@@ -0,0 +1,6 @@
+export * from "./classification.js";
+export * from "./decision.js";
+export * from "./evidence.js";
+export * from "./argument.js";
+export * from "./ground.js";
+export * from "./appeal.js";
