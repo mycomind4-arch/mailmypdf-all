9814e2bf6d69c34d420504e25c252ea0363d62eb
mycomind4-arch <mycomind4@gmail.com>
2026-09-17 17:15:49 -0700

    feat(records-request): register public information route

--- records-request/runtime.ts
+++ records-request/runtime.ts
@@ -13,6 +13,11 @@ export const recordsRequestStartRoutes = Object.freeze([
     load: () => import("./workflows/agency-records-request/start/index"),
   },
   {
+    workflowId: "public-information-request",
+    path: "/records-request/workflows/public-information-request/start/",
+    load: () => import("./workflows/public-information-request/start/index"),
+  },
+  {
     workflowId: "government-documents-request",
     path: "/records-request/workflows/government-documents-request/start/",
     load: () => import("./workflows/government-documents-request/start/index"),
