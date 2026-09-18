2d67363a300910b4ccc066a8eba3e9d5e9901908
mycomind4-arch <mycomind4@gmail.com>
2026-09-17 17:14:55 -0700

    feat(records-request): register government documents route

--- records-request/runtime.ts
+++ records-request/runtime.ts
@@ -13,6 +13,11 @@ export const recordsRequestStartRoutes = Object.freeze([
     load: () => import("./workflows/agency-records-request/start/index"),
   },
   {
+    workflowId: "government-documents-request",
+    path: "/records-request/workflows/government-documents-request/start/",
+    load: () => import("./workflows/government-documents-request/start/index"),
+  },
+  {
     workflowId: "open-records-request",
     path: "/records-request/workflows/open-records-request/start/",
     load: () => import("./workflows/open-records-request/start/index"),
